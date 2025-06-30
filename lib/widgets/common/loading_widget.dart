3c12d19dfdbba542974df87c3d2f3c3947a79533
wiral <wiralodrasaputra07@gmail.com>
2025-06-30 21:26:16 +0700

    Add loading_widget

--- lib/widgets/common/loading_widget.dart
+++ lib/widgets/common/loading_widget.dart
@@ -1,10 +1,312 @@
 import 'package:flutter/material.dart';
 
-class LoadingWidget extends StatelessWidget {
+class LoadingWidget extends StatefulWidget {
+  final String? message;
+  final Color? primaryColor;
+  final double? size;
+
+  const LoadingWidget({
+    super.key,
+    this.message,
+    this.primaryColor,
+    this.size = 50.0,
+  });
+
+  @override
+  State<LoadingWidget> createState() => _LoadingWidgetState();
+}
+
+class _LoadingWidgetState extends State<LoadingWidget>
+    with TickerProviderStateMixin {
+  late AnimationController _rotationController;
+  late AnimationController _scaleController;
+  late AnimationController _fadeController;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _rotationController = AnimationController(
+      duration: Duration(milliseconds: 1500),
+      vsync: this,
+    );
+
+    _scaleController = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    _fadeController = AnimationController(
+      duration: Duration(milliseconds: 800),
+      vsync: this,
+    );
+
+    _rotationAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _rotationController,
+      curve: Curves.linear,
+    ));
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.8,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _scaleController,
+      curve: Curves.easeInOut,
+    ));
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _fadeController,
+      curve: Curves.easeOut,
+    ));
+
+    _rotationController.repeat();
+    _scaleController.repeat(reverse: true);
+    _fadeController.forward();
+  }
+
+  @override
+  void dispose() {
+    _rotationController.dispose();
+    _scaleController.dispose();
+    _fadeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Color primaryColor = widget.primaryColor ?? Color(0xFF8B4513);
+
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildLoadingIndicator(primaryColor),
+            if (widget.message != null) ...[
+              SizedBox(height: 24),
+              _buildLoadingText(),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoadingIndicator(Color primaryColor) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: primaryColor.withOpacity(0.1),
+            blurRadius: 20,
+            offset: Offset(0, 8),
+            spreadRadius: 2,
+          ),
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: AnimatedBuilder(
+        animation: _scaleController,
+        builder: (context, child) {
+          return Transform.scale(
+            scale: _scaleAnimation.value,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                // Outer rotating circle
+                AnimatedBuilder(
+                  animation: _rotationController,
+                  builder: (context, child) {
+                    return Transform.rotate(
+                      angle: _rotationAnimation.value * 2 * 3.14159,
+                      child: Container(
+                        width: widget.size! + 20,
+                        height: widget.size! + 20,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            colors: [
+                              primaryColor.withOpacity(0.8),
+                              primaryColor.withOpacity(0.2),
+                              Colors.transparent,
+                              Colors.transparent,
+                            ],
+                            stops: [0.0, 0.3, 0.7, 1.0],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                // Inner pulsing circle
+                Container(
+                  width: widget.size!,
+                  height: widget.size!,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        primaryColor.withOpacity(0.1),
+                        primaryColor.withOpacity(0.05),
+                        Colors.transparent,
+                      ],
+                      stops: [0.0, 0.7, 1.0],
+                    ),
+                  ),
+                ),
+                // Center icon
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: primaryColor.withOpacity(0.1),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.auto_stories_rounded,
+                    color: primaryColor,
+                    size: 24,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildLoadingText() {
+    return AnimatedBuilder(
+      animation: _scaleController,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: 0.95 + (_scaleAnimation.value * 0.05),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.9),
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 8,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Text(
+              widget.message ?? 'Memuat cerita...',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[700],
+                letterSpacing: 0.3,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+// Variasi loading widget yang lebih simple
+class SimpleLoadingWidget extends StatefulWidget {
+  final String? message;
+
+  const SimpleLoadingWidget({Key? key, this.message}) : super(key: key);
+
+  @override
+  State<SimpleLoadingWidget> createState() => _SimpleLoadingWidgetState();
+}
+
+class _SimpleLoadingWidgetState extends State<SimpleLoadingWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 1200),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CircularProgressIndicator(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return Transform.rotate(
+                angle: _animation.value * 2 * 3.14159,
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF8B4513),
+                        Color(0xFFA0522D),
+                        Colors.transparent,
+                        Colors.transparent,
+                      ],
+                      stops: [0.0, 0.5, 0.5, 1.0],
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              );
+            },
+          ),
+          if (widget.message != null) ...[
+            SizedBox(height: 16),
+            Text(
+              widget.message!,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ],
+      ),
     );
   }
 }
